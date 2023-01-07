93962443b3265ff52505dfc54a79bc481e59272e
ChuffedDom <dom@chuffed.solutions>
2023-01-07 20:38:16 +0000

    Fix Apple Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: '--apple-system',
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
